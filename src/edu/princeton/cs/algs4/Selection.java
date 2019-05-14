d51a98772752557975fd198edcff468456780d2c
Khánh Hoàng <dungkhanh@gmail.com>
2019-05-14 16:41:53 +0700

    remove bin

--- src/edu/princeton/cs/algs4/Selection.java
+++ src/edu/princeton/cs/algs4/Selection.java
@@ -1,23 +1,23 @@
 /******************************************************************************
- *  Compilation:  javac Insertion.java
- *  Execution:    java Insertion < input.txt
+ *  Compilation:  javac Selection.java
+ *  Execution:    java  Selection < input.txt
  *  Dependencies: StdOut.java StdIn.java
  *  Data files:   https://algs4.cs.princeton.edu/21elementary/tiny.txt
  *                https://algs4.cs.princeton.edu/21elementary/words3.txt
- *  
- *  Sorts a sequence of strings from standard input using insertion sort.
- *
+ *   
+ *  Sorts a sequence of strings from standard input using selection sort.
+ *   
  *  % more tiny.txt
  *  S O R T E X A M P L E
  *
- *  % java Insertion < tiny.txt
+ *  % java Selection < tiny.txt
  *  A E E L M O P R S T X                 [ one string per line ]
- *
+ *    
  *  % more words3.txt
  *  bed bug dad yes zoo ... all bad yet
- *
- *  % java Insertion < words3.txt
- *  all bad bed bug dad ... yes yet zoo   [ one string per line ]
+ *  
+ *  % java Selection < words3.txt
+ *  all bad bed bug dad ... yes yet zoo    [ one string per line ]
  *
  ******************************************************************************/
 
@@ -26,19 +26,8 @@ package edu.princeton.cs.algs4;
 import java.util.Comparator;
 
 /**
- *  The {@code Insertion} class provides static methods for sorting an
- *  array using insertion sort.
- *  <p>
- *  This implementation makes ~ 1/2 n^2 compares and exchanges in
- *  the worst case, so it is not suitable for sorting large arbitrary arrays.
- *  More precisely, the number of exchanges is exactly equal to the number
- *  of inversions. So, for example, it sorts a partially-sorted array
- *  in linear time.
- *  <p>
- *  The sorting algorithm is stable and uses O(1) extra memory.
- *  <p>
- *  See <a href="https://algs4.cs.princeton.edu/21elementary/InsertionPedantic.java.html">InsertionPedantic.java</a>
- *  for a version that eliminates the compiler warning.
+ *  The {@code Selection} class provides static methods for sorting an
+ *  array using selection sort.
  *  <p>
  *  For additional documentation, see <a href="https://algs4.cs.princeton.edu/21elementary">Section 2.1</a> of
  *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
@@ -46,10 +35,10 @@ import java.util.Comparator;
  *  @author Robert Sedgewick
  *  @author Kevin Wayne
  */
-public class Insertion {
+public class Selection {
 
     // This class should not be instantiated.
-    private Insertion() { }
+    private Selection() { }
 
     /**
      * Rearranges the array in ascending order, using the natural order.
@@ -57,28 +46,15 @@ public class Insertion {
      */
     public static void sort(Comparable[] a) {
         int n = a.length;
-        for (int i = 1; i < n; i++) {
-            for (int j = i; j > 0 && less(a[j], a[j-1]); j--) {
-                exch(a, j, j-1);
+        for (int i = 0; i < n; i++) {
+            int min = i;
+            for (int j = i+1; j < n; j++) {
+                if (less(a[j], a[min])) min = j;
             }
+            exch(a, i, min);
             assert isSorted(a, 0, i);
         }
         assert isSorted(a);
-    }
-
-    /**
-     * Rearranges the subarray a[lo..hi) in ascending order, using the natural order.
-     * @param a the array to be sorted
-     * @param lo left endpoint (inclusive)
-     * @param hi right endpoint (exclusive)
-     */
-    public static void sort(Comparable[] a, int lo, int hi) {
-        for (int i = lo + 1; i < hi; i++) {
-            for (int j = i; j > lo && less(a[j], a[j-1]); j--) {
-                exch(a, j, j-1);
-            }
-        }
-        assert isSorted(a, lo, hi);
     }
 
     /**
@@ -88,52 +64,17 @@ public class Insertion {
      */
     public static void sort(Object[] a, Comparator comparator) {
         int n = a.length;
-        for (int i = 1; i < n; i++) {
-            for (int j = i; j > 0 && less(a[j], a[j-1], comparator); j--) {
-                exch(a, j, j-1);
+        for (int i = 0; i < n; i++) {
+            int min = i;
+            for (int j = i+1; j < n; j++) {
+                if (less(comparator, a[j], a[min])) min = j;
             }
-            assert isSorted(a, 0, i, comparator);
+            exch(a, i, min);
+            assert isSorted(a, comparator, 0, i);
         }
         assert isSorted(a, comparator);
     }
 
-    /**
-     * Rearranges the subarray a[lo..hi) in ascending order, using a comparator.
-     * @param a the array
-     * @param lo left endpoint (inclusive)
-     * @param hi right endpoint (exclusive)
-     * @param comparator the comparator specifying the order
-     */
-    public static void sort(Object[] a, int lo, int hi, Comparator comparator) {
-        for (int i = lo + 1; i < hi; i++) {
-            for (int j = i; j > lo && less(a[j], a[j-1], comparator); j--) {
-                exch(a, j, j-1);
-            }
-        }
-        assert isSorted(a, lo, hi, comparator);
-    }
-
-
-    // return a permutation that gives the elements in a[] in ascending order
-    // do not change the original array a[]
-    /**
-     * Returns a permutation that gives the elements in the array in ascending order.
-     * @param a the array
-     * @return a permutation {@code p[]} such that {@code a[p[0]]}, {@code a[p[1]]},
-     *    ..., {@code a[p[n-1]]} are in ascending order
-     */
-    public static int[] indexSort(Comparable[] a) {
-        int n = a.length;
-        int[] index = new int[n];
-        for (int i = 0; i < n; i++)
-            index[i] = i;
-
-        for (int i = 1; i < n; i++)
-            for (int j = i; j > 0 && less(a[index[j]], a[index[j-1]]); j--)
-                exch(index, j, j-1);
-
-        return index;
-    }
 
    /***************************************************************************
     *  Helper sorting functions.
@@ -145,9 +86,10 @@ public class Insertion {
     }
 
     // is v < w ?
-    private static boolean less(Object v, Object w, Comparator comparator) {
+    private static boolean less(Comparator comparator, Object v, Object w) {
         return comparator.compare(v, w) < 0;
     }
+        
         
     // exchange a[i] and a[j]
     private static void exch(Object[] a, int i, int j) {
@@ -156,39 +98,38 @@ public class Insertion {
         a[j] = swap;
     }
 
-    // exchange a[i] and a[j]  (for indirect sort)
-    private static void exch(int[] a, int i, int j) {
-        int swap = a[i];
-        a[i] = a[j];
-        a[j] = swap;
-    }
 
    /***************************************************************************
     *  Check if array is sorted - useful for debugging.
     ***************************************************************************/
-    private static boolean isSorted(Comparable[] a) {
-        return isSorted(a, 0, a.length);
-    }
 
-    // is the array a[lo..hi) sorted
+    // is the array a[] sorted?
+    private static boolean isSorted(Comparable[] a) {
+        return isSorted(a, 0, a.length - 1);
+    }
+        
+    // is the array sorted from a[lo] to a[hi]
     private static boolean isSorted(Comparable[] a, int lo, int hi) {
-        for (int i = lo + 1; i < hi; i++)
+        for (int i = lo + 1; i <= hi; i++)
             if (less(a[i], a[i-1])) return false;
         return true;
     }
 
+    // is the array a[] sorted?
     private static boolean isSorted(Object[] a, Comparator comparator) {
-        return isSorted(a, 0, a.length, comparator);
+        return isSorted(a, comparator, 0, a.length - 1);
     }
 
-    // is the array a[lo..hi) sorted
-    private static boolean isSorted(Object[] a, int lo, int hi, Comparator comparator) {
-        for (int i = lo + 1; i < hi; i++)
-            if (less(a[i], a[i-1], comparator)) return false;
+    // is the array sorted from a[lo] to a[hi]
+    private static boolean isSorted(Object[] a, Comparator comparator, int lo, int hi) {
+        for (int i = lo + 1; i <= hi; i++)
+            if (less(comparator, a[i], a[i-1])) return false;
         return true;
     }
 
-   // print array to standard output
+
+
+    // print array to standard output
     private static void show(Comparable[] a) {
         for (int i = 0; i < a.length; i++) {
             StdOut.println(a[i]);
@@ -196,14 +137,14 @@ public class Insertion {
     }
 
     /**
-     * Reads in a sequence of strings from standard input; insertion sorts them;
-     * and prints them to standard output in ascending order.
+     * Reads in a sequence of strings from standard input; selection sorts them; 
+     * and prints them to standard output in ascending order. 
      *
      * @param args the command-line arguments
      */
     public static void main(String[] args) {
         String[] a = StdIn.readAllStrings();
-        Insertion.sort(a);
+        Selection.sort(a);
         show(a);
     }
 }
